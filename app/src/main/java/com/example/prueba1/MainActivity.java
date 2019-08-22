83da1570f13dcbf758bbf2dd5850afa5b5d0c649
brayanvasquez <brayanvasquezsoto@gmail.com>
2019-08-22 16:47:44 -0500

    Nueva tarea asincrona sin usar While para ejecutar en segundo plano

--- app/src/main/java/com/example/prueba1/MainActivity.java
+++ app/src/main/java/com/example/prueba1/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.prueba1;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
         while (true){
             try {
                 Thread.sleep(1000);
@@ -20,20 +30,58 @@ public class MainActivity extends AppCompatActivity {
             }
             fun(50,30);
         }
-    }
-    void fun(int x , int y ){
-        String suma = String.valueOf(x+y);
-        Toast.makeText(MainActivity.this,"Sum: "+suma,Toast.LENGTH_LONG).show();
-        final AlertDialog.Builder dialogo = new AlertDialog.Builder(MainActivity.this);
-        dialogo.setTitle("Resultado Suma");
-        dialogo.setMessage("Sum "+suma);
-        dialogo.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(getApplicationContext(), "Prueba Finalizada!", Toast.LENGTH_LONG).show();
-                dialog.cancel();
+        */
+        Thread tp = new Thread() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        time time = new time();
+                        time.execute();
+                    }
+                });
             }
-        });
-        dialogo.create();
-        dialogo.show();
+        };
+        tp.start();
+    }
+    String fun(int x , int y ){
+        String suma = String.valueOf(x+y);
+        return suma;
+    }
+
+    public void hilo()
+    {
+        try{
+            Thread.sleep(1000);//1000 = 1 Seg.
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void ejecutar()
+    {
+        time time = new time();
+        time.execute();
+    }
+
+    public class time extends AsyncTask<Void,Integer,Boolean>
+    {
+        @Override
+        protected Boolean doInBackground(Void... voids) {
+            for(int i=1; i <= 3; i++)
+            {
+                hilo();
+            }
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            ejecutar();
+            Toast.makeText(MainActivity.this,"Obteniendo Resultado...",Toast.LENGTH_SHORT).show();
+            String suma = fun(50,30);
+            Toast.makeText(MainActivity.this,"Sum: "+suma,Toast.LENGTH_SHORT).show();
+        }
     }
 }
